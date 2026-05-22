31381d33c0592abb16315be56e67a081117ae1f5
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-05-22 14:28:36 +0800

    Auto-commit: Project implementation in progress

--- src/stie/models/peer.py
+++ src/stie/models/peer.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from datetime import datetime
 from enum import Enum
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class PeerStatus(str, Enum):
